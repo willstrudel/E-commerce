c613f86bb7ad0c546beac4fe37ee9a4ab9006ac2
Will <willstruder@gmail.com>
2021-07-27 23:58:36 -0400

    Updated routes to publish to insomnia

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,5 +1,7 @@
--- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
 
--- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+
+USE DATABASE ecommerce_db;
+
+
--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 
 
-
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('sequelize');
@@ -17,12 +16,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-app.get('/', (req, res) => {
-  res.sendStatus(200);
-});
+// app.get('/', (req, res) => {
+//   res.sendStatus(200);
+// });
 
 const dbConnect = require('./config/connection');
 const models = require('./models');
+
+const categorySeeds = require('./seeds/category-seeds');
+const productSeeds = require('./seeds/product-seeds');
+const productTagSeeds = require('./seeds/product-tag-seeds');
+const tagSeeds = require('./seeds/tag-seeds');
+
+const e = require('express');
 models.Category.sync()
   .then(() => {
     return models.Product.sync();
@@ -33,15 +39,36 @@ models.Category.sync()
   .then(() =>{
     return models.ProductTag.sync();
   })
-  .then(() =>{
-    require('./seeds');
+  .then(() => {
+    categorySeeds();
+    productSeeds();
+    productTagSeeds();
+    tagSeeds();
+    return Promise.resolve(true);
   })
   .catch(e => {
     console.error(e);
   });
 
+////////////////////////////////
+// Error Handler
+///////////////////////////////
+
+async function errHandler(err) {
+  console.error(err)
+  process.exit(1)
+}
+
+process.on('uncaughtException', errHandler)
+process.on('unhandledRejection', errHandler)
+
+////////////////////////////////////////////
+
+
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+    console.log(`App listening on port ${PORT}!`);
+}).on('error', err => {
+  console.error(err)
 });
 
